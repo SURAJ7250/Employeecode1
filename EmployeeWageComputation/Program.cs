57ea03c17623b696714d0aa86758c2ee832e9b06
surajbridgelabz <surajnsinha@gmail.com>
2022-08-24 15:57:42 +0530

    [suraj] Add: welcome to employee compuatation

---
+++ EmployeeWageComputation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            //this is entry program of employee wage computation
+            Console.WriteLine("Welcome to Employee wage computation problem");
+        }
+    }
+}
